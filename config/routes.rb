b1943bf79abcca3a8736def249d4931070dc99c9
菊池真弘 <convenient60nls@gmail.com>
2017-04-19 12:14:37 +0900

    ','の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'messages#index'
-  resources :messages only: [:index]
+  resources :messages, only: [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
