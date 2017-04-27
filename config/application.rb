acaf816270364e5249c2ac87d601363749aef542
菊池真弘 <convenient60nls@gmail.com>
2017-04-27 17:36:34 +0900

    devise 日本語化

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module ChatSpace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
+    config.i18n.default_locale= :ja
     config.generators do |g|
       g.test_framework        false
       g.coffee      false
